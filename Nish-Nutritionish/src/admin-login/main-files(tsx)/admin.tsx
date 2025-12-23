ece4d96587c7a6f82b9d957f0f5be851d369501c
Nabeel <nabeelpasha74@gmail.com>
2025-12-23 22:57:52 +0530

    admin css updated

--- Nish-Nutritionish/src/admin-login/main-files(tsx)/admin.tsx
+++ Nish-Nutritionish/src/admin-login/main-files(tsx)/admin.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
-import './admin.css'
+import '../css-file/admin.css'
 
 
 function Admin() {
-    const [credentials, setCredential] = useState ({
+    const [credentials, setCredential] = useState({
         name: "",
         password: ""
     })
@@ -11,66 +11,72 @@ function Admin() {
     const [error, setError] = useState("")
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const {name,value} = e.target
-        setCredential (prev => ({
+        const { name, value } = e.target
+        setCredential(prev => ({
             ...prev,
             [name]: value
         }))
         setError("")
-        
+
 
     }
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-        if (!credentials.name || !credentials.password){
+        if (!credentials.name || !credentials.password) {
             setError("Please fill in all fields")
             return
         }
-        
+
         console.log("Admin Login Attempt:", credentials);
     }
     return (
         <div className='Box1'>
-            <div className="arch1"></div>
-            <div className="arch2"></div>
-            <div className="arch2"></div>
+            <div className='arch'>
+
+                <div className="arc arch1"></div>
+                <div className="arc arch2"></div>
+                <div className="arc arch3"></div>
+            </div>
             <div className="login">
-                <h1 className = 'heading'>Admin Login</h1>
-                <form onSubmit={handleSubmit} className='form1'>
-                    <div className="form-div">
-                        <label htmlFor='name'>Enter your name</label>
-                        <input
-                            type='name'
-                            id='name'
-                            name='name'
-                            value={credentials.name}
-                            onChange={handleChange}
-                            placeholder='Enter your name'/>
-                        <label htmlFor='password'>Enter your password</label>
-                        <input
-                            type='password'
-                            id='password'
-                            name='password'
-                            value={credentials.password}
-                            onChange={handleChange}
-                            placeholder="Enter your password"/>
-                    {error && <div className="error_dialog">{error}</div>}
-                            
-                
-                
-                    </div>
-                
-                
-                </form>
+                <div>
+
+                    <h1 className='heading'>Admin Login</h1>
+                    <form onSubmit={handleSubmit} className='form1'>
+                        <div className="form-div">
+                            <label htmlFor='name' className="name">Enter your name</label>
+                            <input
+                                type='name'
+                                id='name'
+                                name='name'
+                                value={credentials.name}
+                                onChange={handleChange}
+                                placeholder='Enter your name' />
+                            <label htmlFor='password' className="password">Enter your password</label>
+                            <input
+                                type='password'
+                                id='password'
+                                name='password'
+                                value={credentials.password}
+                                onChange={handleChange}
+                                placeholder="Enter your password" />
+                            {error && <div className="error_dialog">{error}</div>}
 
 
 
-                        
+                        </div>
+
+
+                    </form>
+                </div>
+
+
+
+
 
 
             </div>
-            
+
         </div>
     )
 }
